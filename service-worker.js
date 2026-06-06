e8e38d8055f65aa487b9315d53d0514507db4728
dennerbiersack1010 <dennerbiersack1010@gmail.com>
2026-06-06 03:27:02 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
-const CACHE_VERSION = "vault-pay-cache-v2";
-
 self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          return caches.delete(cacheName);
-        })
-      );
-    }).then(() => {
-      return self.clients.claim();
-    })
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => caches.delete(cacheName))
+        );
+      })
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request, {
+      cache: "no-store"
+    })
+  );
 });
