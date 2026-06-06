07ed859b8e0f6a017917cbc85c9805403e658945
dennerbiersack1010 <dennerbiersack1010@gmail.com>
2026-06-06 02:41:16 -0300

    Add service worker registration on DOMContentLoaded

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Vault Pay carregado.");
 
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => {
+        console.log("Service Worker registrado com sucesso.");
+      })
+      .catch((error) => {
+        console.log("Erro ao registrar Service Worker:", error);
+      });
+  }
+});
