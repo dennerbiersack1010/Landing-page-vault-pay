1e312b420a3c167c3b8f8ab4f6a558a1dd5a5071
dennerbiersack1010 <dennerbiersack1010@gmail.com>
2026-06-06 03:44:10 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Vault Pay carregado sem PWA.");
-
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .getRegistrations()
-      .then((registrations) => {
-        registrations.forEach((registration) => {
-          registration.unregister();
-        });
-
-        console.log("PWA removido. Service Workers antigos desativados.");
-      })
-      .catch((error) => {
-        console.log("Erro ao remover Service Worker:", error);
-      });
-  }
-
-  if ("caches" in window) {
-    caches.keys().then((cacheNames) => {
-      cacheNames.forEach((cacheName) => {
-        caches.delete(cacheName);
-      });
-
-      console.log("Caches antigos apagados.");
-    });
-  }
 });
